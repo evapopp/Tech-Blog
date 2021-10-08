c049d0bda93aaad2889595d3eac9dfb47e0b744a
eva <83175995+evapopp@users.noreply.github.com>
2021-10-08 15:35:27 -0700

    errors with seeding data, also my model is not importing correctly

--- seeds/index.js
+++ seeds/index.js
@@ -1,10 +1,13 @@
 const sequelize = require('../config/connection');
-const { Blog } = require('../models');
+const Blog = require('../models');
 
 const blogData = require('./blog_data.json');
 
+
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
+    console.log('blogdata', blogData)
+    
     const blogPost = await Blog.bulkCreate(blogData);
     console.log('\n ---------- Blog Data Seeded! ---------- \n');
     process.exit(0);
--- server.js
+++ server.js
@@ -7,13 +7,15 @@ const routes = require('./controllers')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection');
+
 app.use(express.static(path.join(__dirname, 'public')));
 // Sets up the routes
 app.use(routes);
 
 // Starts the server to begin listening
-app.listen(PORT, () => {
-  console.log('Server listening on: http://localhost:' + PORT);
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
 });
 
 
