d58c4634bef3cf6df9430facf4912fe059733068
eva <83175995+evapopp@users.noreply.github.com>
2021-10-08 18:32:03 -0700

    fixed model issue, added script to index.js file to fix

--- controllers/api/blogRoute.js
+++ controllers/api/blogRoute.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Blog } = require('../../models');
+const Blog = require('../../models');
 
 router.get('/', async (req, res) => {
     try {
--- seeds/index.js
+++ seeds/index.js
@@ -1,23 +1,22 @@
 const sequelize = require('../config/connection');
-const { Blog } = require('../models');
+const Blog = require('../models');
 
-const seedtheblog = require('./blogData')
+// const seedtheblog = require('./blogData')
 
 const blogData = require('./blog_data.json');
 
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
-    console.log(Test)
-    console.log(Blog)
-    await seedtheblog()
-    // await sequelize.sync({ force: true });
-    // console.log('blogdata', blogData)
+    // console.log(Blog)
+    // await seedtheblog()
+    await sequelize.sync({ force: true });
+    console.log('blogdata', blogData)
     
-    // const blogPost = await Blog.bulkCreate(blogData);
+    const blogPost = await Blog.bulkCreate(blogData);
 
-    // console.log(blogPost)
-    // console.log('\n ---------- Blog Data Seeded! ---------- \n');
+    console.log(blogPost)
+    console.log('\n ---------- Blog Data Seeded! ---------- \n');
     process.exit(0);
 };
 
